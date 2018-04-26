06895761a478a562a1255b76732630590c61076a
Loren Cerri <truexpixels@gmail.com>
2018-04-26 16:17:33 -0700

    Patch

--- src/extentions/Message.js
+++ src/extentions/Message.js
@@ -1,17 +1,31 @@
 const hook = require('quick.hook');
 module.exports = Structures => {
-	Structures.extend('Message', DiscordMessage =>
-		class Message extends DiscordMessage { // eslint-disable-line no-unused-vars
-			quickEmbed(text, options = {}) {
-				options = { title: options.title || '', color: options.color || 7648468, deleteAfter: options.deleteAfter || 'null' };
-				this.channel.send({ embed: { title: options.title, description: text, color: options.color } })
-					.then(msg => {
-						if (!isNaN(options.deleteAfter)) msg.delete({ timeout: options.deleteAfter });
-					});
-			}
+    Structures.extend('Message', DiscordMessage =>
+        class Message extends DiscordMessage { // eslint-disable-line no-unused-vars
+            quickEmbed(text, options = {}) {
+                options = {
+                    title: options.title || '',
+                    color: options.color || 7648468,
+                    deleteAfter: options.deleteAfter || 'null',
+                    footer: options.footer || null
+                };
+                this.channel.send({
+                        embed: {
+                            title: options.title,
+                            description: text,
+                            color: options.color,
+                            footer: options.footer
+                        }
+                    })
+                    .then(msg => {
+                        if (!isNaN(options.deleteAfter)) msg.delete({
+                            timeout: options.deleteAfter
+                        });
+                    });
+            }
 
-			quickHook(msg, options = {}) {
-				hook(this.channel, msg, options);
-			}
-		});
+            quickHook(msg, options = {}) {
+                hook(this.channel, msg, options);
+            }
+        });
 };
